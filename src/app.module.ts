ce1b6411e7f4956b99049580f9f413eeca315af8
UNDERBUFFER <ruslanekitsky@gmail.com>
2021-01-16 14:17:32 +0300

    setup db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING ?? '', {
+      useNewUrlParser: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

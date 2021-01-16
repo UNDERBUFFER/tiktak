4c51119d37611ce5212f8867740b9bd5d2b6d719
UNDERBUFFER <ruslanekitsky@gmail.com>
2021-01-16 17:57:12 +0300

    setup models

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Body, Get, Param, Post } from '@nestjs/common';
 import { CreateUsertDto } from './create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService) {}
     @Get(':id')
-    findOne(@Param('id') id: string): string {
-        console.log(`getting user with id ${id}`);
-        return 'get';
+    async findOne(@Param('id') id: string): Promise<string> {
+        const user = await this.userService.get(id)
+        return JSON.stringify(user)
     }
     @Post()
-    create(@Body() createUserDto: CreateUsertDto): string {
-        console.log(`creating user with body ${JSON.stringify(createUserDto)}`);
-        return 'post';
+    async create(@Body() createUserDto: CreateUsertDto): Promise<string> {
+        const user = await this.userService.create(createUserDto)
+        return JSON.stringify(user)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUsertDto } from './create-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUsertDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  async get(id: string): Promise<User> {
+    const user = await this.userModel.findById(id).exec();
+    return user;
+  }
+}

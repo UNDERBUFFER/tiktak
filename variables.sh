717b90ca2b4ca872b52ac923dbca66c79473ff59
UNDERBUFFER <ruslanekitsky@gmail.com>
2021-01-30 14:08:53 +0300

    setup redis

--- variables.sh
+++ variables.sh
@@ -1,3 +1,5 @@
 export MONGODB_CONNECTION_STRING=mongodb://root:toor@localhost:27017
+export REDIS_HOST=localhost
+export REDIS_PORT=26379
 export SMTP_SERVER=smtp://localhost:21025
 export FROM_EMAIL=uover29@gmail.com
